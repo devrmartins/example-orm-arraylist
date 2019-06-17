c9fc6447a5d82383389149233faa8e6575d0242b
Lucas Barros <lucasbarros22@hotmail.com>
2019-06-17 15:14:51 -0300

    Criar classe Usuario

--- src/App/src/Usuario.php
+++ src/App/src/Usuario.php
@@ -8,4 +8,24 @@ class Usuario
 {
     private $nome;
     private $email;
+
+    public function setNome(string $nome) : void
+    {
+        $this->nome = $nome;
+    }
+
+    public function getNome() : string
+    {
+        return $this->nome;
+    }
+
+    public function setEmail(string $email) : void
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail() : string
+    {
+        return $this->email;
+    }
 }
