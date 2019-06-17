5f5af9c81178eb8ac9fa341ff25e66f2bd1031b1
Lucas Barros <lucasbarros22@hotmail.com>
2019-06-17 15:11:00 -0300

    Criar classe Pontuacao

--- src/App/src/Performance/Pontuacao.php
+++ src/App/src/Performance/Pontuacao.php
@@ -8,4 +8,24 @@ class Pontuacao
 {
     private $ano;
     private $pontos = [];
+
+    public function setAno(string $ano) : void
+    {
+        $this->ano = $ano;
+    }
+
+    public function getAno() : string
+    {
+        return $this->ano;
+    }
+
+    public function setPonto(Ponto $ponto) : void
+    {
+        $this->pontos[] = $ponto;
+    }
+
+    public function getPontos() : array
+    {
+        return $this->pontos;
+    }
 }
