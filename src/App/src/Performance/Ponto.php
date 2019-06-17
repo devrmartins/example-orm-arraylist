4e5bd424270a364f0dece7b983d70fdf3562f518
Lucas Barros <lucasbarros22@hotmail.com>
2019-06-17 15:13:07 -0300

    Criar classe Ponto

--- src/App/src/Performance/Ponto.php
+++ src/App/src/Performance/Ponto.php
@@ -10,4 +10,44 @@ class Ponto
     private $nome;
     private $ano;
     private $valor;
+
+    public function setCodigo(string $codigo) : void
+    {
+        $this->codigo = $codigo;
+    }
+
+    public function getCodigo() : string
+    {
+        return $this->codigo;
+    }
+
+    public function setNome(string $nome) : void
+    {
+        $this->nome = $nome;
+    }
+
+    public function getNome() : string
+    {
+        return $this->nome;
+    }
+
+    public function setAno(string $ano) : void
+    {
+        $this->ano = $ano;
+    }
+
+    public function getAno() : string
+    {
+        return $this->ano;
+    }
+
+    public function setValor(decimal $valor) : void
+    {
+        $this->valor = $valor;
+    }
+
+    public function getValor() : decimal
+    {
+        return $this->valor;
+    }
 }
